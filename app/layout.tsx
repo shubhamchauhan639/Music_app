44f42be60185b35b24ffd25bf83f77fbd12f46bd
shubhamchauhan639 <shubhamchauhan12196@gmail.com>
2026-06-02 17:57:23 +0530

    building Navbar of music app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +26,13 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+     className={`dark ${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+
+     <body className="min-h-screen bg-black text-white">
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
